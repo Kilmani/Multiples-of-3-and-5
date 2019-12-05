865995b0615e9ea96b6adf9abb32f8d0cf80fbbe
Kilmani <maksimovvlad2010@gmail.com>
2019-12-05 13:55:22 +0300

    Update Multiples_of_3_and_5.py

--- Multiples_of_3_and_5.py
+++ Multiples_of_3_and_5.py
@@ -3,12 +3,12 @@ i=0
 summary=0
 while i < 1000:
     if (i%3 == 0) & (i/3 != 0):
-        summary = summary + i
+        summary+=i
         print(i)
 
     if (i%5 == 0) & (i/5 != 0):
-        summary = summary + i
+        summary+=i
         print(i)
-    i=i+1
+    i+=1
     
 print(summary)
